d8351c52f40b20b24690c09fb0195d6959d3e038
devpctwo <devpctwo@ERPdevpctwo.myguest.virtualbox.org>
2026-01-25 15:11:09 +0530

    converted property and property unit to custom=0

---
+++ rentclear/rentclear/doctype/property/property.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Thronesix and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Property", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ rentclear/rentclear/doctype/property_unit/test_property_unit.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Thronesix and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPropertyUnit(FrappeTestCase):
+	pass
